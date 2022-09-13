7c3f2013170fd0a62b3c82287f76700fe31dd26a
sameerpanigrahy <100825520+sameerpanigrahy@users.noreply.github.com>
2022-09-13 23:37:27 +0530

    bug free code

--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const PORT=process.env.PORT||3000
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 
 mongoose.connect("mongodb+srv://panigrahisameer_200:iklsSoxrtvpy4JOK@cluster0.kyd9m93.mongodb.net/group-56", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+.then( () => console.log("MongoDb is connected"),err => console.log(err))
+
 
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(  PORT, function () {
+    console.log('Express app running on port ' + PORT)
 });
+

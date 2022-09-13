c34e35054cf63f14f012d19298d66be531f4d672
sameerpanigrahy <100825520+sameerpanigrahy@users.noreply.github.com>
2022-09-13 23:35:59 +0530

    bug free code

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,33 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const authorController=require("../controllers/authorsController")
-const blogController=require("../controllers/blogsController")
-const middleWare=require("../Middleware/Middleware")
+const { loginAuthor,createAuthor}=require("../controllers/authorsController")
+const {createBlog,getblog,updateBlog,deleteBlog,deleteBlogByQuery}=require("../controllers/blogsController")
+const {authenticate}=require("../Middleware/Middleware")
 
 
 router.get("/test-me",function(req,res){
     res.status(200).send({msg:"All ok"})
 })
 //-------------------------------API for Create Author-----------------------//
-router.post("/authors",authorController.createAuthor)
+router.post("/authors",createAuthor)
 
 
 //-------------------------------API for  Author Login-----------------------//
 
-router.post("/login",authorController.loginAuthor)
+router.post("/login",loginAuthor)
 
 //-----------------------API for Create Blog-----------------------------//
-router.post("/blogs",middleWare.authenticate,blogController.createBlog)
+router.post("/blogs",authenticate,createBlog)
+
 
 //---------------------API for Fetch Blog--------------------------//
-router.get("/getblogs",middleWare.authenticate,blogController.getblog)
+router.get("/getblogs",authenticate,getblog)
 
 //---------------------API for Update  Blog------------------//
-router.put("/blogs/:blogId",middleWare.authenticate,blogController.updateBlog)
+router.put("/blogs/:blogId",authenticate,updateBlog)
 
 //---------------API for Delete Blog---------------------//
-router.delete("/blogs/:blogId",middleWare.authenticate,blogController.deleteBlog)
-router.delete("/delete",middleWare.authenticate,blogController.deleteBlogByQuery)
+router.delete("/blogs/:blogId",authenticate,deleteBlog)
+router.delete("/delete",authenticate,deleteBlogByQuery)
 
 
 //......................................FOR restore  all data-------//
